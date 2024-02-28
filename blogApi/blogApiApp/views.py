d289d65f1cb51ac7c2f8a87ca62c10dce2d2db3b
khang <khang@tungvts-iMac-191.local>
2024-02-28 16:03:25 +0700

    done

--- blogApi/blogApiApp/views.py
+++ blogApi/blogApiApp/views.py
@@ -15,3 +15,52 @@ def GetAllPosts(request):
     get_posts = Post.objects.all()
     serializer = PostSerializer(get_posts, many = True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def CreatePost(request):
+    data = request.data
+    serializer = PostSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"Success": "The post has successfully created"}, status=201)
+    else:
+        return Response(serializer.errors, status=400)
+    
+@api_view(['DELETE'])
+def DeletePost(request):
+    post_id = request.data.get('post_id')
+    try:
+        post = Post.objects.get(id=post_id)
+        post.delete()
+        return Response({"Success": "The post was successfully deleted"}, status=200)
+    except Post.DoesNotExist:
+        return Response({"Error": "The post does not exist"}, status=404)
+    
+@api_view(['GET'])
+def GetPost(request):
+    post_id = request.data.get('post_id')
+    try:
+        post = Post.objects.get(id=post_id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=200)
+    except Post.DoesNotExist:
+        return Response({"Error": "The post does not exist"}, status=404)
+    
+@api_view(['PUT'])
+def UpdatePost(request): 
+    post_id = request.data.get('post_id')
+    get_new_title = request.data.get('new_title')
+    get_new_content = request.data.get('_new_content')
+    try:
+        post = Post.objects.get(id=post_id)
+
+        if get_new_title:
+            post.title = get_new_title
+        if get_new_content:
+            post.title = get_new_content
+        
+        post.save()
+        return Response({"Success": "The post was successfully updated"}, status=200)
+    except Post.DoesNotExist:
+        return Response({"Error": "The post does not exist"}, status=404)
